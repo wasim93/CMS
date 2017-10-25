d3e5298fbb4c6801e1ab56a03e1cff495e51baa2
wasim <muhammad.wasim93@gmail.com>
2017-10-25 23:52:13 +0500

    Delete + Partial Edit

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,6 @@
 <?php include "includes/index_header.php"; ?>
 
-<?php                          // Getting Values Categories Into Table
-                              $query = "SELECT cat_id,cat_title FROM categories ORDER BY cat_id ASC";
-                              $select_categories = $connection->query($query); 
-?>
+
 
 <body>
 
@@ -23,7 +20,7 @@
                             <small>Author</small>
                         </h1>
                     
-                    <div class="col-xs-4"><!-- Adding Categories Form -->
+                    <div class="col-xs-5"><!-- Adding Categories Form -->
 
                         <?php 
 
@@ -38,27 +35,63 @@
                                 }
                                 else
                                 {
-                                    $query = "INSERT INTO categories(cat_title) VALUES('$cat_title') ";
+                                    $query = "INSERT INTO categories (cat_title) VALUES('$cat_title') ";
                                     $create_category_query = $connection->query($query); 
-                                    if (mysqli_affected_rows($connection) == 1) 
-                                    {
-                                        redirectMe (APP_URL.'admin/categories.php');
-                                    }
+                                    header('location:categories.php');
+                                    // if (mysqli_affected_rows($connection) == 1) 
+                                    // {
+                                    //     redirectMe (APP_URL.'admin/categories.php');
+                                    // }
                                 }
                         }
 
                          ?>
-
+                                    <!-- Adding Category Form -->
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Add New Category</label>
                                 <input class="form-control" type="text" name="cat_title">
-
                             </div>
-
-                            <div class="form-group">
-                                
+                            <div class="form-group">                
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+
+                            </div>      
+                        </form>
+
+                        <hr>
+                                    <!-- Updating Category Form -->
+                         <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat_title">Edit Category</label>
+
+                            <?php    //Update Query
+                                      
+                                    if (isset($_GET['edit'])) 
+                                    {
+                                     $cat_id = $_GET['edit'];
+                                     $query = "SELECT cat_id,cat_title FROM categories WHERE cat_id = $cat_id";
+                                     $select_categories = $connection->query($query);
+
+                                     while($row = $select_categories->fetch_assoc()) 
+                                        { 
+                                        $cat_id = $row['cat_id'];
+                                        $cat_title = $row['cat_title'];
+                                        
+                            ?>
+                                         <input type="text" name="cat_title" class="form-control" 
+                                         value="<?php if(isset($cat_title))
+                                         {
+                                         echo $cat_title; }?>" >
+                                         
+                                        
+
+                                       <?php }} ?> 
+                            
+                                
+                                <!-- <input class="form-control" type="text" name="cat_title"> -->
+                            </div>
+                            <div class="form-group">                
+                                <input class="btn btn-primary" type="submit" name="submit" value="Update">
 
                             </div>      
                         </form>
@@ -66,7 +99,7 @@
                     </div><!-- End Adding Categories Form -->
 
                     
-                    <div class="col-xs-8"><!-- Category Table -->
+                    <div class="col-xs-7"><!-- Category Table -->
 
                         <table class="table table-bordered table-hover">
                          
@@ -79,8 +112,11 @@
                             </thead>
                             <tbody>
                                 
-                                <?php  
-                                while($row = $select_categories->fetch_assoc()) 
+                                <?php
+                                 // Getting Values Categories Into Table
+                              $query = "SELECT cat_id,cat_title FROM categories ORDER BY cat_id ASC";
+                              $select_categories = $connection->query($query); 
+                                 while($row = $select_categories->fetch_assoc()) 
                                { 
                                 $cat_id = $row['cat_id'];
                                 $cat_title = $row['cat_title'];
@@ -88,11 +124,32 @@
                                     <tr>                                
                                      <td><?php echo $cat_id; ?></td>
                                      <td><?php echo $cat_title; ?></td>
-                                     <td><a href ='categories.php?delete=<?$cat_id;?>'> Delete </a>
+                                     <td> 
+                                     <a href ='categories.php?edit=<?php echo $cat_id;?> '>Edit |</a>
+                                     <a href ='categories.php?delete=<?php echo $cat_id;?> '> Delete </a>
                                      </td>
                                     </tr>
 
-                            <?php } ?>
+                            <?php }
+
+                                    // Delete Query
+                                    if (isset($_GET['delete'])) {
+                                        
+                                       //$get_delete_id = $_GET['delete'];
+                                       $query = "DELETE FROM categories WHERE cat_id = '".$_GET['delete']."' ";
+                                       $delete_query = $connection->query($query); 
+                                       if ($connection->query($query))
+                                        {
+                                            header('location:categories.php');
+                                            exit();
+                                        } else 
+                                        {
+                                        echo "Error: " . $query . "<br>" . $connection->error;
+                                        }       
+                                    }
+
+                                            
+                             ?>
             
                                 
                             </tbody>
